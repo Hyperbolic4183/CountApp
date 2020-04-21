f0d6b3846307f2da7b9d6fd944015e36aa107b07
otsuka.amane <otsuka.amane@gmail.com>
2020-04-21 17:31:42 +0900

    テキストカラーの変更と型変換について

--- Swift5CountUp1/ViewController.swift
+++ Swift5CountUp1/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+    var countNumber:Int = 0
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBAction func downButton(_ sender: Any) {
+        countNumber += 1
+        let convert = String(countNumber)
+        if countNumber % 3 == 0{
+            numberLabel.text = convert
+            numberLabel.textColor = UIColor.yellow
+            print("\(countNumber)")
+        } else {
+            numberLabel.text = convert
+             numberLabel.text = convert
+            numberLabel.textColor = UIColor.white
+        }
+    }
+    @IBAction func upButton(_ sender: Any) {
+        countNumber -= 1
+        let convert = String(countNumber)
+        if countNumber % 3 == 0{
+            numberLabel.textColor = UIColor.yellow
+            numberLabel.text = convert
+        } else {
+            numberLabel.text = convert
+            numberLabel.textColor = UIColor.white
+        }
+        
+        numberLabel.text = convert
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var convert = String(countNumber)
+        numberLabel.text = convert
+        numberLabel.textColor = UIColor.yellow
     }
-
 
 }
 
